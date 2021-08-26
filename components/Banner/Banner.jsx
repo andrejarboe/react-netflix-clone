79edebb058cd0fb513800aa6d346ed69df5dd03a
andrejarboe <andrejarboe@gmail.com>
2021-08-26 15:12:43 -0400

    banner img is mvp

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -28,11 +28,10 @@ export default function Banner() {
         <header
             className="banner"
             style={{
-                backgroundSize: 'cover',
+        
                 backgroundImage: `url(
                     "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
                 )`,
-                backgroundPosition: "center center"
             }}
         >{/* background image */}
             <div className="banner__contents">
---
+++ pages/test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../components/Banner/Banner'
+
+export default function test() {
+    return (
+        <div>
+            <div className="test">
+            <Banner />
+            </div>
+        </div>
+    )
+}
